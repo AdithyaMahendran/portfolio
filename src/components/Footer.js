9d9c2aadbe154668720bd4047025404606d63b1d
Adithya M <adithya.m@in.abb.com>
2024-03-15 20:20:45 +0530

    portfolio

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Container } from "react-bootstrap";
+
+function Footer() {
+  return (
+    <Container fluid className="footer">
+    </Container>
+  );
+}
+
+export default Footer;
